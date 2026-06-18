f7b32d78fba69153ca56ed0483f5ee1e47ad3c6d
levisamuell <demolitiongaming2005@gmail.com>
2026-06-18 10:02:46 +0530

    removed conflicting port and cleaned the workflow

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
-
-const PORT = Number(process.env.PORT) || 5000;
-
-const PORT = Number(process.env.PORT) || 3000; // default port for dev
+const PORT = Number(process.env.PORT) || 3000; //Removed the conflicting port ie port 5000 and set it to 3000 which is the default port for development. This allows for easier testing and development without conflicts with other services that may be using port 5000.
 
 
 module.exports = {
