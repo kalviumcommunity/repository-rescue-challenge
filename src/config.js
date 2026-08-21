20cc9f062df9e33604b2f8041ad2039d898319a3
dhairyajangir <thinkelite@hotmail.com>
2026-08-21 10:32:14 +0530

    feature: adding feature flag feature

--- src/config.js
+++ src/config.js
@@ -1,9 +1,19 @@
 
 // const PORT = Number(process.env.PORT) || 5000;
 
+
 const PORT = Number(process.env.PORT) || 3000; // default port for dev
 
 
 module.exports = {
   PORT,
 };
+
+// adding a feature 
+const FEATURE_FLAG = process.env.FEATURE_FLAG || false;
+
+module.exports = {
+  PORT,
+  FEATURE_FLAG,
+};
+
