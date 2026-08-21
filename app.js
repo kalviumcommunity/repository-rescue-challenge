82a6785dee52f04786b29b0bf83d5205b43fb06f
avishkar durgade <avishkardurgade@gmail.com>
2026-08-21 10:46:13 +0530

    add GET /about endpoint for app information

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const { PORT } = require('./src/config');
+const express = require("express");
+const { PORT } = require("./src/config");
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`DevOps assessment app is running on port ${PORT}`);
 });
 
-app.get('/health', (req, res) => {
-  res.send('OK');
-}
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is a DevOps assessment app");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
